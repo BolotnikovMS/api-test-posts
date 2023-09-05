87f8b349331a42789bb584b78065dbd5ff248b9d
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-05 21:44:53 +0500

    feat: Рефакторинг и новые методы
    Добавлен интерфейс для фильтров и перечисление для сортировки. Добавлен метод для просмотра поста и метод для обновления поста

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -3,16 +3,26 @@ import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
 
+enum orderBy {
+  'acs' = 'asc',
+  'desc' = 'desc'
+}
+
+interface IQueryParams {
+  page: number
+  size: number
+  sort: string
+  order: orderBy
+}
+
 export default class PostsController {
   public async index({ response, request }: HttpContextContract) {
     try {
-      const { page, size, sortBy } = request.qs()
-      console.log(request.qs())
+      const { page = 1, size = -1, sort, order } = request.qs() as IQueryParams
 
       const posts = await Post.query()
-        .if(sortBy, query => query.orderBy(sortBy[0] === '-' ? sortBy.substring(1) : sortBy, sortBy[0] === '-' ? 'desc' : 'asc'))
-        // .if(page && size, query => query.paginate(page, size))
-        .paginate(page ? page : 1, size ? size : -1)
+        .if(sort && order, query => query.orderBy(sort, orderBy[order]))
+        .paginate(page, size)
 
       posts.baseUrl('/api/v1/posts')
 
@@ -54,11 +64,52 @@ export default class PostsController {
     }
   }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ response, params }: HttpContextContract) {
+    try {
+      const post = await Post.findBy('slug', params['post(slug)'])
+
+      if (post) {
+        return response.status(200).json(post)
+      }
+
+      return response.status(404).json({ message: 'Не найдено!' })
+    } catch (error) {
+      console.log(error)
+
+      return response.status(500).json({ message: 'Произошла ошибка при выполнении запроса!' })
+    }
+  }
 
   public async edit({}: HttpContextContract) {}
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, params }: HttpContextContract) {
+    try {
+      const post = await Post.findBy('slug', params['post(slug)'])
+
+      if (post) {
+        const postSchema = schema.create({
+          topicId: schema.number.optional(),
+          title: schema.string.optional([rules.trim(), rules.minLength(3), rules.maxLength(200), rules.escape()]),
+          body: schema.string.optional([rules.trim(), rules.minLength(5), rules.escape()]),
+        })
+        const messages: CustomMessages = {
+          required: 'Поле {{ field }} является обязательным.',
+          minLength: 'Минимальная длинна {{ field }} - {{ options.minLength }} символа.',
+          maxLength: 'Максимальная длинна {{ field }} - {{ options.maxLength }} символа.',
+        }
+        const validatedData = await request.validate({ schema: postSchema, messages })
+        const updPost = await post.merge(validatedData).save()
+
+        return response.status(200).json(updPost)
+      }
+
+      return response.status(404).json({ message: 'Не найдено!' })
+    } catch (error) {
+      console.log(error)
+
+      return response.status(400).json({ error })
+    }
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
