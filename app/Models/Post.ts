3c328fb9d05d6feacd68d1c2a6d91d612eb705ce
BolotnikovMS <bolotnikovms@yandex.ru>
2023-09-24 18:47:40 +0500

    feat: :sparkles: Добавлена связь

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,7 +1,8 @@
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 
 import { DateTime } from 'luxon'
 import { slugify } from '@ioc:Adonis/Addons/LucidSlugify'
+import User from 'App/Models/User'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
@@ -31,4 +32,7 @@ export default class Post extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
