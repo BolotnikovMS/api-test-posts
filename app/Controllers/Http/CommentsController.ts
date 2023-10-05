13f8892a4f8dbaf468634c078b37fdccc16f19ae
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-05 15:50:41 +0500

    feat: :sparkles: Добавлены методы
    Добавлены методы для работы с комментариями.

--- app/Controllers/Http/CommentsController.ts
+++ app/Controllers/Http/CommentsController.ts
@@ -1,8 +1,10 @@
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
+
 import Comment from 'App/Models/Comment'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class CommentsController {
-  public async index({ response, request }: HttpContextContract) {
+  public async index({ response }: HttpContextContract) {
     try {
       const comments = await Comment.query()
 
@@ -12,11 +14,66 @@ export default class CommentsController {
     }
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response, auth }: HttpContextContract) {
+    try {
+      const commentSchema = schema.create({
+        postId: schema.number(),
+        commentBody: schema.string([rules.trim(), rules.minLength(3), rules.maxLength(200)])
+      })
+      const messages: CustomMessages = {
+        required: 'Поле {{ field }} является обязательным.',
+        minLength: 'Минимальная длинна {{ field }} - {{ options.minLength }} символа.',
+        maxLength: 'Максимальная длинна {{ field }} - {{ options.maxLength }} символа.',
+      }
+      const validatedData = await request.validate({ schema: commentSchema, messages })
+      const comment = await Comment.create({userId: auth.user?.id, ...validatedData})
+
+      return response.status(201).json(comment)
+    } catch (error) {
+      return response.status(400).json(error.messages.errors[0])
+    }
+  }
 
   public async show({}: HttpContextContract) {}
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, params }: HttpContextContract) {
+    try {
+      const comment = await Comment.findBy('slug', params['comment(slug)'])
 
-  public async destroy({}: HttpContextContract) {}
+      if (comment) {
+        const commentSchema = schema.create({
+          commentBody: schema.string([rules.trim(), rules.minLength(3), rules.maxLength(200)])
+        })
+        const messages: CustomMessages = {
+          required: 'Поле {{ field }} является обязательным.',
+          minLength: 'Минимальная длинна {{ field }} - {{ options.minLength }} символа.',
+          maxLength: 'Максимальная длинна {{ field }} - {{ options.maxLength }} символа.',
+        }
+        const validatedData = await request.validate({ schema: commentSchema, messages })
+        const updComment = await comment.merge({postId: comment.postId, ...validatedData}).save()
+
+        return response.status(200).json(updComment)
+      }
+
+      return response.status(404).json({ message: 'Не найдено!' })
+    } catch (error) {
+      return response.status(400).json(error.messages.errors[0])
+    }
+  }
+
+  public async destroy({ response, params }: HttpContextContract) {
+    try {
+      const comment = await Comment.findBy('slug', params['comment(slug)'])
+
+      if (comment) {
+        await comment.delete()
+
+        return response.status(204)
+      }
+
+      return response.status(404).json({ message: 'Не найдено!' })
+    } catch (error) {
+
+    }
+  }
 }
