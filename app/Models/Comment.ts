49a569cf77d8f2ceae76d8df266fe22ea8eadffc
BolotnikovMS <bolotnikovms@yandex.ru>
2023-10-05 17:54:50 +0500

    feat: :sparkles: Добавлены отношения с Post, User через belongsto

--- app/Models/Comment.ts
+++ app/Models/Comment.ts
@@ -1,6 +1,8 @@
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 
 import { DateTime } from 'luxon'
+import Post from 'App/Models/Post'
+import User from 'App/Models/User'
 import { slugify } from '@ioc:Adonis/Addons/LucidSlugify'
 
 export default class Comment extends BaseModel {
@@ -28,4 +30,10 @@ export default class Comment extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => Post)
+  public post: BelongsTo<typeof Post>
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
